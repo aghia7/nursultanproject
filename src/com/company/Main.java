91f81cfb7e77cd2775f78df10eca2130faef5fe2
askar.khaimuldin <askar.khaimuldin@astanait.edu.kz>
2021-01-18 21:25:53 +0600

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,21 @@ public class Main {
     private static Scanner scanner;
 
     public static void main(String[] args) throws Exception {
-        Student student = new Student();
-        student.setName("Balenbay");
-        System.out.println(student.getName());
+        File file = new File("C:\\Users\\aghia\\OneDrive\\Рабочий стол\\file1.txt");
+        scanner = new Scanner(file);
+        Shape shape = new Shape();
+
+        while(scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String a = line.substring(0, line.indexOf(","));
+            String b = line.substring(line.indexOf(" ") + 1);
+
+            Point point = new Point(Integer.parseInt(a), Integer.parseInt(b));
+            shape.addPoint(point);
+        }
+
+        System.out.println(shape.calculatePerimeter());
+        System.out.println(shape.getLongestSide());
+        System.out.println(shape.getAverageLength());
     }
 }
